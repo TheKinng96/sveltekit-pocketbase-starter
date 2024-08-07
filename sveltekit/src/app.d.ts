eddce94576a6a6363864388d2739de06a1d4f25a
G3N <fengyuan.yap@gmail.com>
2024-08-07 21:17:04 +0900

    save file

--- sveltekit/src/app.d.ts
+++ sveltekit/src/app.d.ts
@@ -1,9 +1,14 @@
+import type { TypedPocketBase, AuthSystemFields } from '$lib/types/pocketbase-types';
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			pb: TypedPocketBase;
+			user: AuthSystemFields | undefined;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
---
+++ sveltekit/src/routes/+layout.server.ts
@@ -0,0 +1,10 @@
+import type { LayoutServerLoad } from './$types'
+import { redirect } from '@sveltejs/kit'
+
+export const load = (async ({ locals }) => {
+	// if (!locals.user) {
+	// 	throw redirect(302, '/login')
+	// }
+
+	return {}
+}) satisfies LayoutServerLoad
